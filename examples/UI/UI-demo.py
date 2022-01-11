6984726106927aa82f74ba6a3c3b9a4ba617816e
Denis <obliquetsvetkov@gmail.com>
2022-01-11 17:14:03 +0200

    added functionality
    functions to do, if structure

--- examples/UI/UI-demo.py
+++ examples/UI/UI-demo.py
@@ -1,10 +1,18 @@
-value = input("Welcome to the menu. Options are listed below:",
-              "\n\t 1. Roll \n\t 2. Sweep \n\t 3. Scroll \n Type the",
-              "number of your choice and press Enter.") #\n - newline; \t - tab
+value = input('''Welcome to the menu. Options are listed below:,
+              \t 1.Roll
+              \t 2. Sweep
+              \t 3. Scroll
+              Type the number of your choice and press Enter.''') #\n - newline; \t - tab
 
 #print("The value you input is:", value)
 #print(f'it is of type {type(value)}.')
 
+def func1(val):
+    return val**val
+def func2(val):
+    return val**val
+def func3(val):
+    return val**val
 
 while True:
     if value.isdigit() == True: # .isdigit() 
@@ -14,3 +22,11 @@ while True:
         value=input("invalid input, please provide an integer:") #ask for a new value
 #print("The converted is:", value)
 #print(f'it is of type {type(value)}.')
+
+#compare numeric value to choices available, perform assicoated function or sequence.
+if value == 1:
+    print(func1(value))
+elif value == 2:
+    print(func2(value))
+elif value == 3:
+    print(func3(value))
