fc126794ae3d56d13636722277a47aacd7a44062
Denis <obliquetsvetkov@gmail.com>
2022-01-11 19:01:29 +0200

    reverse stuff

--- examples/basic_animations.py
+++ examples/basic_animations.py
@@ -30,19 +30,14 @@ while led<60:
 
 led = 0
 while led<60: #scroll all rows at the same time
-    for rows in range(6):
-        leds[led + rows*60] = (0,0,255) 
-    #leds[led+  0] = (0,0,255) #row 1 - 0-60
-    #leds[led+ 60] = (0,0,255) #row 2 - 0+60 - 59+60 (60-119)
-    #leds[led+120] = (0,0,255)
-    #leds[led+180] = (0,0,255) #this can be done in 2 lines instead of 6
-    #leds[led+240] = (0,0,255)
-    #leds[led+300] = (0,0,255)
+    for rows in range(3): #first three rows left to right
+        leds[led + rows*60] = (0,0,255)
+    for rows in range(3,6): #last three rows reversed (right to left)
+        leds[59-led + rows*60] = (50,50,255)
     client.put_pixels(leds)
     time.sleep(.1)
     led = led + 1
 
-# refactor the last example to use less code
 # reverse the last example.
 # do a scroll from the middle to the outside - two pixels moving away from each other.
 # reverse the scroll from the middle
