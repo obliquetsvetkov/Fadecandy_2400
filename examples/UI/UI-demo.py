44371c0b14c00efa4fb61672f6ad1529e7c7d2f3
Denis <obliquetsvetkov@gmail.com>
2022-01-11 16:51:39 +0200

    updated with string format and int conversion

--- examples/UI/UI-demo.py
+++ examples/UI/UI-demo.py
@@ -1,3 +1,9 @@
-value = input()
+value = input("Welcome to the menu. Options are listed below:")
 
-print(value)
+print("The value you input is:", value)
+print(f'it is of type {type(value)}.')
+
+value = int(value)
+
+print("The converted is:", value)
+print(f'it is of type {type(value)}.')
