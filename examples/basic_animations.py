37e4350d6ab7bf8221188c9ade05430f1ae33beb
Denis <obliquetsvetkov@gmail.com>
2022-01-11 18:15:06 +0200

    added scrolling all rows together

--- examples/basic_animations.py
+++ examples/basic_animations.py
@@ -16,7 +16,26 @@ for led in range(60): #pick out indeces: led = 0,1,2,3...
 
 led = 0
 while led<60:
-    leds[led] = (0,255,0)
+    leds[59-led] = (0,255,0)
     time.sleep(.1)
     client.put_pixels(leds)
     led = led + 1 #or reverse if you want
+
+#led = 59
+#while led>=0:
+#    leds[led] = (0,255,0)
+#    time.sleep(.1)
+#    client.put_pixels(leds)
+#    led = led - 1 #or reverse if you want
+
+led = 0
+while led<60: #scroll all rows at the same time
+    leds[led] = (0,0,255) #row 1 - 0-60
+    leds[led+60] = (0,0,255) #row 2 - 0+60 - 59+60 (60-119)
+    leds[led+120] = (0,0,255)
+    leds[led+180] = (0,0,255) #this can be done in 2 lines instead of 6
+    leds[led+240] = (0,0,255)
+    leds[led+300] = (0,0,255)
+    client.put_pixels(leds)
+    time.sleep(.1)
+    led = led + 1    
