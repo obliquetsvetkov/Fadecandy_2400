a97935191af1b93c659361ba2a9425ac354d3696
Denis <obliquetsvetkov@gmail.com>
2022-01-11 18:57:52 +0200

    better scroll

--- examples/basic_animations.py
+++ examples/basic_animations.py
@@ -30,12 +30,14 @@ while led<60:
 
 led = 0
 while led<60: #scroll all rows at the same time
-    leds[led] = (0,0,255) #row 1 - 0-60
-    leds[led+60] = (0,0,255) #row 2 - 0+60 - 59+60 (60-119)
-    leds[led+120] = (0,0,255)
-    leds[led+180] = (0,0,255) #this can be done in 2 lines instead of 6
-    leds[led+240] = (0,0,255)
-    leds[led+300] = (0,0,255)
+    for rows in range(6):
+        leds[led + rows*60] = (0,0,255) 
+    #leds[led+  0] = (0,0,255) #row 1 - 0-60
+    #leds[led+ 60] = (0,0,255) #row 2 - 0+60 - 59+60 (60-119)
+    #leds[led+120] = (0,0,255)
+    #leds[led+180] = (0,0,255) #this can be done in 2 lines instead of 6
+    #leds[led+240] = (0,0,255)
+    #leds[led+300] = (0,0,255)
     client.put_pixels(leds)
     time.sleep(.1)
     led = led + 1
