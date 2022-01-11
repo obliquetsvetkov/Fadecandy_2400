420aa4251e1abd41916b7f0b696da2e8f705b40d
Denis <obliquetsvetkov@gmail.com>
2022-01-11 17:08:10 +0200

    loop for datatype validation
    check if data is int

--- examples/UI/UI-demo.py
+++ examples/UI/UI-demo.py
@@ -1,9 +1,16 @@
-value = input("Welcome to the menu. Options are listed below:")
+value = input("Welcome to the menu. Options are listed below:",
+              "\n\t 1. Roll \n\t 2. Sweep \n\t 3. Scroll \n Type the",
+              "number of your choice and press Enter.") #\n - newline; \t - tab
 
-print("The value you input is:", value)
-print(f'it is of type {type(value)}.')
+#print("The value you input is:", value)
+#print(f'it is of type {type(value)}.')
 
-value = int(value)
 
-print("The converted is:", value)
-print(f'it is of type {type(value)}.')
+while True:
+    if value.isdigit() == True: # .isdigit() 
+        value = int(value)
+        break # on correct value datatype: exit the loop
+    else:
+        value=input("invalid input, please provide an integer:") #ask for a new value
+#print("The converted is:", value)
+#print(f'it is of type {type(value)}.')
