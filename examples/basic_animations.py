a8fefcb52f4ee43f443a8c8a3c716a993cc616da
Denis <obliquetsvetkov@gmail.com>
2022-01-11 18:04:03 +0200

    added two alternatives for scrolling

--- examples/basic_animations.py
+++ examples/basic_animations.py
@@ -8,14 +8,15 @@ client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 client.put_pixels(leds)
 
-leds[10] = (255, 0, 0)
+#for led in leds: # pick out an element: led = (255,255,255)
+for led in range(60): #pick out indeces: led = 0,1,2,3...
+    leds[led] = (255,0,0)
+    time.sleep(.1)
+    client.put_pixels(leds)
 
-time.sleep(3)
-client.put_pixels(leds)
-client.put_pixels(leds)
-
-leds[10+60] = (255, 0, 0)
-
-time.sleep(3)
-client.put_pixels(leds)
-client.put_pixels(leds)
+led = 0
+while led<60:
+    leds[led] = (0,255,0)
+    time.sleep(.1)
+    client.put_pixels(leds)
+    led = led + 1 #or reverse if you want
