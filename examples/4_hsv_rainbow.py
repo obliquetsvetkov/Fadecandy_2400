0bf3a0976d2ac1236dc6fab8eec3e7fd779fbc28
obliquetsvetkov <obliquetsvetkov@gmail.com>
2022-01-18 17:13:40 +0200

    fade to individual pixel linear gradient

--- examples/4_hsv_rainbow.py
+++ examples/4_hsv_rainbow.py
@@ -3,17 +3,21 @@ from time import sleep
 import colorsys
 
 client = opc.Client('localhost:7890')
+leds = [(0,0,0)]*360
 
 s = 1.0 ##maximum colour
 v = 1.0 ##maximum brightness
 
 for hue in range(360):
     rgb_fractional = colorsys.hsv_to_rgb(hue/360.0, s, v) #colorsys returns floats between 0 and 1
+    print(rgb_fractional)
     r_float = rgb_fractional[0] #extract said floating point numbers
     g_float = rgb_fractional[1]
     b_float = rgb_fractional[2]
 
     rgb = (r_float*255, g_float*255, b_float*255) #make new tuple with corrected values
-    client.put_pixels([rgb]*360) #send out
+    print(rgb)
+    leds[hue] = rgb 
+    client.put_pixels(leds) #send out
 
     sleep(0.03) #20ms 
--- examples/basic_animations.py
+++ examples/basic_animations.py
@@ -28,15 +28,15 @@ client.put_pixels(leds)
 # #    client.put_pixels(leds)
 # #    led = led - 1 #or reverse if you want
 
-# led = 0
-# while led<60: #scroll all rows at the same time
-#     for rows in range(3): #first three rows left to right
-#         leds[led + rows*60] = (0,0,255)
-#     for rows in range(3,6): #last three rows reversed (right to left)
-#         leds[59-led + rows*60] = (50,50,255)
-#     client.put_pixels(leds)
-#     time.sleep(.1)
-#     led = led + 1
+led = 0
+while led<60: #scroll all rows at the same time
+    for rows in range(3): #first three rows left to right
+        leds[led + rows*60] = (0,0,255)
+    for rows in range(3,6): #last three rows reversed (right to left)
+        leds[59-led + rows*60] = (rows*20,50,255)
+    client.put_pixels(leds)
+    time.sleep(.1)
+    led = led + 1
 
 # reverse the last example.
 # do a scroll from the middle to the outside - two pixels moving away from each other.
