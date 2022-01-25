2770d2b9678fd381e679a718884a6aca015541c1
Denis <obliquetsvetkov@gmail.com>
2022-01-25 19:19:40 +0200

    Update tkinter_practical_example.py

--- examples/tkinter_practical_example.py
+++ examples/tkinter_practical_example.py
@@ -1,6 +1,7 @@
 #opc stuff
 import opc
 import time
+import random
 
 leds = [(255,255,255)]*360
 client = opc.Client('localhost:7890')
@@ -18,14 +19,13 @@ def anim1():
     led = 0
     while led<60: #scroll all rows at the same time
         for rows in range(3): #first three rows left to right
-            leds[led + rows*60] = (0,0,255)
+            leds[led + rows*60] = (0,0,random.randint(127,255))
         for rows in range(3,6): #last three rows reversed (right to left)
-            leds[59-led + rows*60] = (50,50,255)
+            leds[59-led + rows*60] = (50,50,random.randint(127,255))
         client.put_pixels(leds)
         time.sleep(0.1)
         led = led + 1
-    client.put_pixels([(255,255,255)]*360)
-
+        
 def placeholder1():
     pass
 
