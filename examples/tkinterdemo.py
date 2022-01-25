496e1c6cdf661fac995604d9149a8f773786d2d8
Denis <obliquetsvetkov@gmail.com>
2022-01-25 17:34:57 +0200

    entry,button,keypress

--- examples/tkinterdemo.py
+++ examples/tkinterdemo.py
@@ -3,12 +3,23 @@ import tkinter as tk
 window = tk.Tk() #object that is root window
 window.title('TKInter hello world')
 
-text = tk.Label(text = 'Hello world')
-text2 = tk.Label(text = 'two')
-text3 = tk.Label(text = 'three')
-
+text = tk.Label(text = "What's your name?", foreground = 'white',
+                background = 'blue', width = 30, height = 3)
 text.pack()
-text2.pack()
-text3.pack()
+
+entry = tk.Entry(width = 30)
+entry.pack()
+
+def save_and_print():
+    name = entry.get() #denis, type 'string'
+    entry.delete(0, tk.END)
+    print(name)
+
+button = tk.Button(text = 'Submit', width = 30, height = 5,
+                   command = save_and_print)
+button.pack(padx = 5, pady = 5)
+
+#window.bind('<Return>', command = save_and_print)
+window.bind('<Return>', lambda event:save_and_print()) #if func need parameters
 
 window.mainloop() # start event loop, method on top of 'window'
