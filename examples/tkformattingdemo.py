94c5b93768c02aad258f674ee5c02dfdbdb8c384
Denis <obliquetsvetkov@gmail.com>
2022-01-25 17:57:09 +0200

    update with grid

--- examples/tkformattingdemo.py
+++ examples/tkformattingdemo.py
@@ -2,12 +2,12 @@ import tkinter as tk
 
 window = tk.Tk()
 
-frame1 = tk.Frame(width = 100, height = 100, bg = 'green')
-frame2 = tk.Frame(width = 20, height = 20, bg = 'red')
-frame3 = tk.Frame(width = 50, height = 50, bg = 'blue')
-
-frame1.pack(side = tk.LEFT, fill = tk.BOTH, expand = True)
-frame2.pack(side = tk.LEFT, fill = tk.BOTH, expand = True)
-frame3.pack(side = tk.LEFT, fill = tk.BOTH, expand = True)
+for row in range(3):
+    for col in range(3):
+        frame = tk.Frame(master = window, relief = tk.RAISED, borderwidth = 1)
+        frame.grid(row=row, column = col, padx = 5, pady = 5)
+        label = tk.Label(master = frame, text = f"{row}, {col}")
+        label.pack()
+        
 
 window.mainloop()
