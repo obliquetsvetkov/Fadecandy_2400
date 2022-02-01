7eca81cdbf4c0fc21b8929d5369291d8212e3154
Denis <obliquetsvetkov@gmail.com>
2022-02-01 19:42:08 +0200

    Update arduino_demo_one_way.py

--- examples/arduino_demo_one_way.py
+++ examples/arduino_demo_one_way.py
@@ -1,11 +1,12 @@
-import serial
+import serial # if missing - in command prompt or terminal - pip install pyserial
 import time
 
-arduino=serial.Serial('COM5', 9600) #check com on windows; for mac, check what /dev/tty port it uses.
-
+arduino= serial.Serial('COM5', 9600) #check com on windows; for mac, check what /dev/tty port it uses.
 time.sleep(2)
 
+#example for multiple input
+#pot1, pot2, ldr = int(data.split(',')) #  "255, 407, 0" = ["255", "407", "0"] = [255, 407, 0]
 
 while True:
-	data = int(arduino.readline().decode('utf-8')) # works for default println behaviour. use str() if you're sending large datasets.
+	data = arduino.readline().decode('utf-8') # works for default println behaviour. use str() if you're sending large datasets.
 	print(data)
